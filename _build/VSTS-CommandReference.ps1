985f87995f28574777febe1c723552152753e2bf
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-02-21 14:48:40 +0100

    Added building blocks for VSTS fun

---
+++ _build/VSTS-CommandReference.ps1
@@ -0,0 +1,130 @@
+Write-Host "###############################################################"
+Write-Host " - "
+Write-Host "Importing Required modules"
+Write-Host "  Importing PSFramework"
+Import-Module PSFramework -Force
+Write-Host "  Importing PSModuleDevelopment"
+Import-Module PSModuleDevelopment -Force
+Write-Host "  Importing PSUtil"
+Import-Module PSUtil -Force
+Write-Host " - "
+Write-Host "###############################################################"
+Write-Host " - "
+
+$commandReferenceBasePath = "$($env:SYSTEM_DEFAULTWORKINGDIRECTORY)\documentation\commands"
+
+#region PSFramework
+$moduleName = "PSFramework"
+$excludedCommands = @("New-PSFTeppCompletionResult")
+Write-PSFMessage -Level Host -Message "Processing $moduleName"
+Write-PSFMessage -Level Host -Message "  Creating list of commands to process"
+$commands = Get-Command -Module $moduleName -CommandType Function | Select-Object -ExpandProperty Name | Where-Object { $_ -notin $excludedCommands } | Sort-Object
+Write-PSFMessage -Level Host -Message "  $($commands.Count) commands found"
+
+Write-PSFMessage -Level Host -Message "  Creating markdown help files"
+Remove-Item "$($commandReferenceBasePath)\$($moduleName)" -Recurse
+$null = New-Item "$($commandReferenceBasePath)\$($moduleName)" -ItemType Directory
+$null = New-MarkdownHelp -Command $commands -OutputFolder "$($commandReferenceBasePath)\$($moduleName)"
+
+Write-PSFMessage -Level Host -Message "  Creating index file"
+Set-Content -Path "$($commandReferenceBasePath)\$($moduleName).html" -Value @"
+# $moduleName Command Reference
+
+"@ -Encoding UTF8
+
+foreach ($command in $commands)
+{
+	Add-Content -Path "$($commandReferenceBasePath)\$($moduleName).html" -Value " - [$command]($($moduleName)/$command.html)"
+}
+Write-PSFMessage -Level Host -Message "Finished processing $moduleName"
+#endregion PSFramework
+
+#region PSModuleDevelopment
+$moduleName = "PSModuleDevelopment"
+$excludedCommands = @()
+Write-PSFMessage -Level Host -Message "Processing $moduleName"
+Write-PSFMessage -Level Host -Message "  Creating list of commands to process"
+$commands = Get-Command -Module $moduleName -CommandType Function | Select-Object -ExpandProperty Name | Where-Object { $_ -notin $excludedCommands } | Sort-Object
+Write-PSFMessage -Level Host -Message "  $($commands.Count) commands found"
+
+Write-PSFMessage -Level Host -Message "  Creating markdown help files"
+Remove-Item "$($commandReferenceBasePath)\$($moduleName)" -Recurse
+$null = New-Item "$($commandReferenceBasePath)\$($moduleName)" -ItemType Directory
+$null = New-MarkdownHelp -Command $commands -OutputFolder "$($commandReferenceBasePath)\$($moduleName)"
+
+Write-PSFMessage -Level Host -Message "  Creating index file"
+Set-Content -Path "$($commandReferenceBasePath)\$($moduleName).html" -Value @"
+# $moduleName Command Reference
+
+"@ -Encoding UTF8
+
+foreach ($command in $commands)
+{
+	Add-Content -Path "$($commandReferenceBasePath)\$($moduleName).html" -Value " - [$command]($($moduleName)/$command.html)"
+}
+Write-PSFMessage -Level Host -Message "Finished processing $moduleName"
+#endregion PSModuleDevelopment
+
+#region PSUtil
+$moduleName = "PSUtil"
+$excludedCommands = @()
+Write-PSFMessage -Level Host -Message "Processing $moduleName"
+Write-PSFMessage -Level Host -Message "  Creating list of commands to process"
+$commands = Get-Command -Module $moduleName -CommandType Function | Select-Object -ExpandProperty Name | Where-Object { $_ -notin $excludedCommands } | Sort-Object
+Write-PSFMessage -Level Host -Message "  $($commands.Count) commands found"
+
+Write-PSFMessage -Level Host -Message "  Creating markdown help files"
+Remove-Item "$($commandReferenceBasePath)\$($moduleName)" -Recurse
+$null = New-Item "$($commandReferenceBasePath)\$($moduleName)" -ItemType Directory
+$null = New-MarkdownHelp -Command $commands -OutputFolder "$($commandReferenceBasePath)\$($moduleName)"
+
+Write-PSFMessage -Level Host -Message "  Creating index file"
+Set-Content -Path "$($commandReferenceBasePath)\$($moduleName).html" -Value @"
+# $moduleName Command Reference
+
+"@ -Encoding UTF8
+
+foreach ($command in $commands)
+{
+	Add-Content -Path "$($commandReferenceBasePath)\$($moduleName).html" -Value " - [$command]($($moduleName)/$command.html)"
+}
+Write-PSFMessage -Level Host -Message "Finished processing $moduleName"
+#endregion PSUtil
+
+Write-Host " - "
+Write-Host "###############################################################"
+Write-Host " - "
+
+branch = $env:BUILD_SOURCEBRANCHNAME
+Write-PSFMessage -Level Host -Message "Applying documentation to repository"
+
+git add .
+git commit -m "VSTS Library Compile ***NO_CI***"
+#$errorMessage = git push "https://$env:SYSTEM_ACCESSTOKEN@github.com/sqlcollaborative/dbatools.git" head:$branch 2>&1
+#if ($LASTEXITCODE -gt 0) { throw $errorMessage }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Write-PSFMessage -Level Host -Message "Git: Adding changes"
+git add -A .
+Write-PSFMessage -Level Host -Message "Git: Creating Commit"
+git commit -m "VSTS PSFramework Command Reference Update ***NO_CI***"
+Write-PSFMessage -Level Host -Message "Git: Pushing changes"
+#$errorMessage = git push https://github.com/PowershellFrameworkCollective/PowershellFrameworkCollective.github.io.git master 2>&1
+$errorMessage = git push origin master 2>&1
+$errorMessage = $errorMessage -replace ([regex]::Escape("$($env:SYSTEM_ACCESSTOKEN)")), "***"
+Write-PSFMessage -Level Host -Message $errorMessage
+
+if ($LASTEXITCODE -gt 0) { throw $errorMessage }
